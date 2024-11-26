242d2645000c7b8ab12d283f87bc350a7bca9c53
3AJeniffer <00001110398177sp@al.educacao.sp.gov.br>
2024-11-26 14:22:44 -0300

    aul 06

---
+++ Perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+        'Programação',
+        'O que é JavaScript?',
+        'O JavaScript é uma linguagem de programação'
+    )
+    
+    criaCartao(
+        'Geografia',
+        'Qual a capital da França?',
+        'A capital da França é Paris'
+    )
+    
+    criaCartao(
+        'Programação',
+        'Qual é seu nome?',
+        'Meu nome e .......'
+    )
